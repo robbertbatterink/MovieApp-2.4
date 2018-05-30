d18e7425fed67aa9d36d8260efe957aa15003833
Robbert Batterink <r.t.batterink@st.hanze.nl>
2018-05-30 12:49:13 +0200

    fix movieslists

--- src/components/Movieslist.js
+++ src/components/Movieslist.js
@@ -1,43 +1,24 @@
 import React from "react";
 
-const getDetailPage = () => {
-	console.log("Get Detail");
-}
-/*
-const getDetailPage = () =>{
-	const updatePage = React.createClass({
-		getInitialState: function(){
-			return { showResults: false};
-		},
-		onClick
-	})
-
-	<div id="MovieDetail">
-
-		<p>Detail page : Lorem ipsum</p>
-
-	</div>
-};
-*/
 const Movieslist = props => (
-	<div class="movieList_container">
+	<div className="movieList_container">
 
 		<h3>Recently recommended</h3>
-		<ul class="movie_list">
+		<ul className="movie_list">
 
 			<li>
-				<div class="innerMovie">
-					<div class="innerMovie_Image" onClick={getDetailPage}></div>
+				<div className="">
+					<div className="innerMovie_Image" onClick={props.click}></div>
 				</div>
 			</li>
 			<li>
-				<div class="innerMovie im2">
-					<div class="innerMovie_Image2" onClick={getDetailPage}></div>
+				<div className="">
+					<div className="im2" onClick={props.click}></div>
 				</div>
 			</li>
 			<li>
-				<div class="innerMovie im3">
-					<div class="innerMovie_Image3" onClick={getDetailPage}></div>
+				<div className="">
+					<div className="im3" onClick={props.click}></div>
 				</div>
 			</li>
 
@@ -47,14 +28,3 @@ const Movieslist = props => (
 );
 
 export default Movieslist
-
-
-/**
-<form onSubmit={props.getWeather}>
-	<input type="text" name="city" placeholder="City..."/>
-	<input type="text" name="country" placeholder="Country..."/>
-	<button>Get Movie</button>
-</form>
-);
-
-*/
